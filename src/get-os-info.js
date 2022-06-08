5f852a4221aa84cc1ccd6d3b403af19b8e402ceb
Vladimir Bessonov <bessmy@gmail.com>
2022-06-08 13:43:30 +0400

    feat: add get-os-info

---
+++ src/get-os-info.js
@@ -0,0 +1,49 @@
+import os from 'os';
+
+const getOsInfo = (arg) => {
+  let message = '';
+
+  switch (arg) {
+    case '--EOL':
+      message = `End of line: ${JSON.stringify(os.EOL)}`;
+      break;
+
+    case '--cpus':
+      message = getCpusInfo();
+      break;
+
+    case '--homedir':
+      message = `Homedir: "${os.homedir()}"`;
+      break;
+
+    case '--username':
+      message = `Username: ${os.userInfo().username}`;
+      break;
+
+    case '--architecture':
+      message = `Architecture: ${os.arch()}`;
+      break;
+
+    default:
+      message = 'Invalid input';
+      break;
+  }
+
+  console.log(message);
+};
+
+const getCpusInfo = () => {
+  const cpuArr = os.cpus();
+  const cpuLength = cpuArr.length;
+  let infoMessage = `Amount of CPUS: ${cpuLength}\n`;
+
+  cpuArr.forEach((cpu, i) => {
+    const cpuModel = cpu.model.trim();
+    const cpuRateInGhz = cpu.speed / 1000;
+    infoMessage += `${i + 1}) Model: ${cpuModel}, Clock rate: ${cpuRateInGhz} GHz\n`;
+  });
+
+  return infoMessage;
+};
+
+export default getOsInfo;
