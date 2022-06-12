1a4d79ee01fd780257c5e36b7c00ae060ec187bd
Vladimir Bessonov <bessmy@gmail.com>
2022-06-12 13:01:37 +0400

    feat: error if copy to itself

--- src/copy-file.js
+++ src/copy-file.js
@@ -5,6 +5,9 @@ const copyFile = async (workingDir, pathToFile, pathToNewDirectory) => {
   const baseFilePath = path.resolve(workingDir, pathToFile);
   const filename = path.basename(baseFilePath);
   const newFilePath = path.join(workingDir, pathToNewDirectory, filename);
+  if (path.dirname(baseFilePath) === path.dirname(newFilePath)) {
+    throw new Error('Cannot copy file to itself');
+  }
   const file = await fsProm.open(baseFilePath, 'r');
   const newFile = await fsProm.open(newFilePath, 'w');
   const readStream = file.createReadStream();
