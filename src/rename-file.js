e5e532c45a840075c3460e73728e63c70d0a610a
Vladimir Bessonov <bessmy@gmail.com>
2022-06-12 18:17:36 +0400

    feat: add error when file exist

--- src/rename-file.js
+++ src/rename-file.js
@@ -1,10 +1,17 @@
 import fsProm from 'fs/promises';
 import path from 'path';
+import { constants } from 'node:fs';
 
 const renameFile = async (workingDir, pathToFile, newFilename) => {
   const baseFilePath = path.resolve(workingDir, pathToFile);
   const renamedFilePath = path.resolve(workingDir, newFilename);
-  await fsProm.rename(baseFilePath, renamedFilePath);
+
+  try {
+    await fsProm.access(renamedFilePath, constants.F_OK);
+    console.log('Operation failed: File with new filename already exists');
+  } catch (error) {
+    await fsProm.rename(baseFilePath, renamedFilePath);
+  }
 };
 
 export default renameFile;
