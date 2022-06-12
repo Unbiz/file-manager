9a28ed183cf892b06f34800c54bbfa8edf642ff5
Vladimir Bessonov <bessmy@gmail.com>
2022-06-12 13:29:09 +0400

    feat: get hash with stream

--- src/get-hash.js
+++ src/get-hash.js
@@ -1,12 +1,17 @@
 import crypto from 'crypto';
 import fsProm from 'fs/promises';
 import path from 'path';
+import util from 'util';
+import stream from 'stream';
 
 const getHash = async (workingDir, pathToFile) => {
   const fullFilePath = path.resolve(workingDir, pathToFile);
-  const fileText = await fsProm.readFile(fullFilePath, 'utf8');
-  const hash = crypto.createHash('sha256').update(fileText).digest('hex');
-  console.log(`Hash of ${fullFilePath} is ${hash}`);
+  const file = await fsProm.open(fullFilePath, 'r');
+  const readStream = file.createReadStream();
+  const hash = crypto.createHash('sha256').setEncoding('hex');
+  const pipeline = util.promisify(stream.pipeline);
+  await pipeline(readStream, hash);
+  console.log(`Hash of ${fullFilePath} is ${hash.read()}`);
 };
 
 export default getHash;
