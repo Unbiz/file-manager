de38e28a1dbc7ae9066ec18b1ffeb6ca369078f1
Vladimir Bessonov <bessmy@gmail.com>
2022-06-12 18:17:06 +0400

    feat: add finised promisify

--- src/print-file-content.js
+++ src/print-file-content.js
@@ -1,17 +1,20 @@
 import fsProm from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import util from 'util';
+import stream from 'stream';
 
 const printFileContent = async (workingDir, filename) => {
   const filePath = path.resolve(workingDir, filename);
-  const file = await fsProm.open(filePath, 'r');
-  const readable = file.createReadStream();
 
-  readable.on('end', () => {
-    process.stdout.write(os.EOL);
-  });
-
+  const readable = fs.createReadStream(filePath);
   readable.pipe(process.stdout);
+
+  const finished = util.promisify(stream.finished);
+  await finished(readable);
+
+  process.stdout.write(os.EOL);
 };
 
 export default printFileContent;
